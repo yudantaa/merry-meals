2fffef231753640e2a0af4878294d83b1714124d
romiardanaputra <romiardana21@gmail.com>
2025-05-18 12:16:00 +0800

    chore: add color boiler plate template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,54 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-      "./node_modules/flowbite/**/*.js"
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
     ],
 
     theme: {
-      extend: {
-        fontFamily: {
-          'poppins': ['Poppins', 'sans-serif'] 
-        },
+        extend: {
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
 
-        colors: {
-          'yellow-rgba-84': 'rgba(255, 222, 108, 0.84)',
-        }
-      },
+            colors: {
+                "yellow-rgba-84": "rgba(255, 222, 108, 0.84)",
+                primary: "#FFCE01",
+                secondary: "#FFFCF0",
+                accent: "A07C00",
+                accentSecondary: '#EDB01C',
+                dark: "#282222",
+                light: "#fffcf0",
+            },
+        },
     },
-    
+
     daisyui: {
         themes: [
-          {
-            mytheme: {
+            {
+                mytheme: {
+                    primary: "#FF7B54",
 
-              "primary": "#FF7B54",
+                    secondary: "#FFB26B",
 
-              "secondary": "#FFB26B",
+                    accent: "#FFD56B",
 
-              "accent": "#FFD56B",
+                    neutral: "#3D4451",
 
-              "neutral": "#3D4451",
+                    "base-100": "#FFFFFF",
 
-              "base-100": "#FFFFFF",
+                    info: "#3ABFF8",
 
-              "info": "#3ABFF8",
+                    success: "#36D399",
 
-              "success": "#36D399",
+                    warning: "#FBBD23",
 
-              "warning": "#FBBD23",
-
-              "error": "#F87272",
+                    error: "#F87272",
+                },
             },
-          },
         ],
-      },
-    plugins: [require("daisyui","flowbite")]
-  }
+    },
+    plugins: [require("daisyui", "flowbite")],
+};
