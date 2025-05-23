d6da5dbe04942eb78e37b676052fd4141b7cf1cd
romiardanaputra <romiardana21@gmail.com>
2025-05-23 10:53:06 +0800

    refactor: adjust route and folder for naming convention

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Pages\AboutController;
+use App\Http\Controllers\Pages\BlogController;
+use App\Http\Controllers\Pages\ContactController;
+use App\Http\Controllers\Pages\DonationController;
+use App\Http\Controllers\Pages\IndexController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,18 +19,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+  return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::group(['middleware' => 'web'], function () {
+  Route::get('/', [IndexController::class, 'index'])->name('index');
+  Route::get('/about', [AboutController::class, 'index'])->name('about');
+  Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+
+  Route::group(['prefix' => 'donation'], function () {
+    Route::get('/', [DonationController::class, 'index'])->name('donation');
+    Route::get('/create', [DonationController::class, 'create'])->name('donation.create');
+  });
+
+  Route::group(['prefix' => 'blog'], function(){
+    Route::get('/', [BlogController::class, 'index'])->name('blog');
+  });
+});
+
+
+require __DIR__ . '/auth.php';
