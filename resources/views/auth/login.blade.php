b1d51fce2870e1d6fb0da8b994d9216c927d1ed6
romiardanaputra <romiardana21@gmail.com>
2025-05-23 10:55:17 +0800

    style: whitespace by prettier

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
   {{--
     <form method="POST" action="{{ route('login') }}">
     @csrf
-
+    
     <!-- Email Address -->
     <div>
     <x-input-label for="email" :value="__('Email')" />
@@ -21,11 +21,11 @@
     />
     <x-input-error :messages="$errors->get('email')" class="mt-2" />
     </div>
-
+    
     <!-- Password -->
     <div class="mt-4">
     <x-input-label for="password" :value="__('Password')" />
-
+    
     <x-text-input
     id="password"
     class="mt-1 block w-full"
@@ -34,10 +34,10 @@
     required
     autocomplete="current-password"
     />
-
+    
     <x-input-error :messages="$errors->get('password')" class="mt-2" />
     </div>
-
+    
     <!-- Remember Me -->
     <div class="mt-4 block">
     <label for="remember_me" class="inline-flex items-center">
@@ -50,7 +50,7 @@
     <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
     </label>
     </div>
-
+    
     <div class="mt-4 flex items-center justify-end">
     @if (Route::has('password.request'))
     <a
@@ -60,7 +60,7 @@
     {{ __('Forgot your password?') }}
     </a>
     @endif
-
+    
     <x-primary-button class="ml-3">
     {{ __('Log in') }}
     </x-primary-button>
